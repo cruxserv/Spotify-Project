68fde404387c35247ca9e93b3df5ebbbeaf4821b
John-Carlos Sanabria <johncarlos.sanabria@gmail.com>
2022-01-26 18:07:10 -0800

    containerization

--- website/__init__.py
+++ website/__init__.py
@@ -3,34 +3,47 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
-# Basic database setup
+# Setup ENVironment variable for working locally (testing) vs full deployment to cloud
+ENV = "dev"
+
+
+# Basic database setup for development
 db = SQLAlchemy()
-DB_Name = 'database.db'
+DB_Name = "database.db"
 
 # Flask Web App setup
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'a supersecret'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_Name}'
+    app.config["SECRET_KEY"] = "a supersecret"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    if ENV == "dev":
+        app.debug = True
+        app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_Name}"
+    else:
+        app.debug = False
+        app.config[
+            "SQLALCHEMY_DATABASE_URI"
+        ] = "postgres://djstreamadmin:admin123@djstreams.postgres.database.azure.com"
+
     db.init_app(app)
 
-# Blueprint setup
+    # Blueprint setup
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
 
-# Connect database requirements
+    # Connect database requirements
     from .models import User
 
     create_database(app)
 
-# Login app settings
+    # Login app settings
     login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
+    login_manager.login_view = "auth.login"
     login_manager.init_app(app)
 
     @login_manager.user_loader
@@ -39,10 +52,11 @@ def create_app():
 
     return app
 
+
 # Creates database if none exists
 
 
 def create_database(app):
-    if not path.exists('website/' + DB_Name):
+    if not path.exists("website/" + DB_Name):
         db.create_all(app=app)
         print("Created Database!")
